9ced45ebea1208e067a3013abd0c0733d24957f1
Admiraltonkey <AdmiralTonkey@gmail.com>
2019-10-22 15:37:03 +0300

    Updates

---
+++ precache-manifest.01fdb8041017e000d77397f5b3d911e6.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44525e69da9f57bf101120e5b56a7e7d",
+    "url": "/PortfolioReact/index.html"
+  },
+  {
+    "revision": "e16c777a81fa278ed2b4",
+    "url": "/PortfolioReact/static/css/main.d8599a0f.chunk.css"
+  },
+  {
+    "revision": "b2352e43ff2be8d76832",
+    "url": "/PortfolioReact/static/js/2.6655adb9.chunk.js"
+  },
+  {
+    "revision": "e16c777a81fa278ed2b4",
+    "url": "/PortfolioReact/static/js/main.90685aba.chunk.js"
+  },
+  {
+    "revision": "7bd5c4bd0c91ad5a3b20",
+    "url": "/PortfolioReact/static/js/runtime~main.07fd605d.js"
+  },
+  {
+    "revision": "224d96086ce079b54ea4fb7001b7f3db",
+    "url": "/PortfolioReact/static/media/A8hXfU75KeY.224d9608.jpg"
+  },
+  {
+    "revision": "431abcff1b5afefe31e7d804bb4aa89f",
+    "url": "/PortfolioReact/static/media/Brandi.431abcff.jpg"
+  },
+  {
+    "revision": "ff60d17e87eb74160dd67b176a9e75db",
+    "url": "/PortfolioReact/static/media/Count.ff60d17e.png"
+  },
+  {
+    "revision": "e0b396601d91eddd2b9b31f04c8560c4",
+    "url": "/PortfolioReact/static/media/Instagram_icon.e0b39660.png"
+  },
+  {
+    "revision": "6b6290a683a80e62efcb707fbef5bba7",
+    "url": "/PortfolioReact/static/media/Linkedin_icon.6b6290a6.png"
+  },
+  {
+    "revision": "392355aa36a5ab069cbca832bf6f7c99",
+    "url": "/PortfolioReact/static/media/VK_icon.392355aa.png"
+  },
+  {
+    "revision": "db7abc48318cb889d32c1fb2a0c7751b",
+    "url": "/PortfolioReact/static/media/dog.db7abc48.png"
+  },
+  {
+    "revision": "0c5248f60b59abfbfa00f4fbf3c7b3e2",
+    "url": "/PortfolioReact/static/media/gulp.0c5248f6.svg"
+  },
+  {
+    "revision": "ce6e5ba07db5030872f1c4934b16a166",
+    "url": "/PortfolioReact/static/media/html_icon.ce6e5ba0.svg"
+  },
+  {
+    "revision": "6e8f68df1509ab98bffab08b6f001c56",
+    "url": "/PortfolioReact/static/media/react-icon.6e8f68df.png"
+  },
+  {
+    "revision": "034768c860323a5ff87673582200fb70",
+    "url": "/PortfolioReact/static/media/react_todo_list.034768c8.jpg"
+  },
+  {
+    "revision": "a31e291cf737552cfe583ee6f6a2b1e7",
+    "url": "/PortfolioReact/static/media/reactjs-tutorial-700x280.a31e291c.png"
+  },
+  {
+    "revision": "59c467536111a981d904a050d335c2be",
+    "url": "/PortfolioReact/static/media/redux.59c46753.png"
+  },
+  {
+    "revision": "b446ca5a397c1ef6fb94cd02c4e94335",
+    "url": "/PortfolioReact/static/media/sass-1.b446ca5a.svg"
+  },
+  {
+    "revision": "4fb728226b7662381ecdaa9fa2f81eb9",
+    "url": "/PortfolioReact/static/media/social_icons.4fb72822.jpg"
+  },
+  {
+    "revision": "0cbc8000febcd852f969e7af471d1308",
+    "url": "/PortfolioReact/static/media/teamProject.0cbc8000.jpeg"
+  }
+]);
